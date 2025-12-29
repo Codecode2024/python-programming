67f597f3fe238848afc88bbc8f0a82fd9e9dea8a
codecode2024 <weberb2024@yahoo.com>
2025-12-29 23:16:28 +0800

    Step6: index page

--- config/settings.py
+++ config/settings.py
@@ -24,9 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
-
-
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -44,6 +41,7 @@ DJANGO_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 ]
 
 APPLICATION_APPS = ['pages.apps.PagesConfig',
@@ -51,10 +49,12 @@ APPLICATION_APPS = ['pages.apps.PagesConfig',
                     'doctors.apps.DoctorsConfig'
                     ]
 
-INSTALLED_APPS = DJANGO_APPS + APPLICATION_APPS
+THIRD_PARTY_APPS = ["debug_toolbar",]
 
+INSTALLED_APPS = DJANGO_APPS + APPLICATION_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -146,4 +146,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
+
 
--- config/urls.py
+++ config/urls.py
@@ -19,12 +19,13 @@ from django.urls import path, include
 
 from django.conf.urls.static import static
 from django.conf import settings 
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('',include('pages.urls',namespace='pages')),
     path('listings/', include('listings.urls', namespace='listings')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + debug_toolbar_urls()
 
 admin.site.site_header = "Clinic Administration"
 admin.site.site_title = "Clinic Admin Portal"
